980791fc05926aaaf920085b3d19d4e88f7e1a86
yongsookim.com <yongsookim.com@gmail.com>
2018-04-21 15:04:47 +0900

    init

---
+++ src/main/java/org/zerock/Boot6Application.java
@@ -0,0 +1,12 @@
+package org.zerock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot6Application.class, args);
+	}
+}
