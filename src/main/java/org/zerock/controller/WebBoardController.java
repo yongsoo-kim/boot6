b6ac99eb3e5303162ea10d60f8a5dd287d32f474
yongsookim.com <yongsookim.com@gmail.com>
2018-04-22 21:20:50 +0900

    initial page and layout

---
+++ src/main/java/org/zerock/controller/WebBoardController.java
@@ -0,0 +1,20 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.java.Log;
+
+@Controller
+@RequestMapping("/boards/")
+@Log
+public class WebBoardController {
+
+	@GetMapping("/list")
+	public void list() {
+		log.info("list() called");
+	}
+	
+	
+}
